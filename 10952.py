23e0f6e7778777b2805a10fd1c369214c674662e
JELLO <JELLO_9504@users.noreply.github.com>
2021-01-04 02:16:37 +0900

    Lv. Apple #2

---
+++ 10952.py
@@ -0,0 +1,18 @@
+#코드 길이 169 B
+while True:
+    try :
+        A, B = map(int, input().split())
+        if A and B != 0:
+            print(A+B)
+        else:
+            break
+    except :
+        break
+
+# 코드 길이 112 B
+while True:
+    A, B = map(int, input().split())
+    if A and B != 0:
+        print(A+B)
+    else:
+        break
---
+++ 11021.py
@@ -0,0 +1,11 @@
+# 코드 길이 105 B
+T=int(input())
+for i in range(T):
+    A, B=map(int,input().split())
+    print("Case #"+str(i+1)+':', A+B)
+
+# 코드 길이 108 B
+T=int(input())
+for i in range(1, T+1):
+    A, B=map(int,input().split())
+    print("Case #"+str(i)+':', A+B)
