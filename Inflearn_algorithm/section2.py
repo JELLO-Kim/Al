cabaf79b6fbaec56bb207680917f680d76ad03cf
chaehyun <cogus950403@gmail.com>
2024-01-17 23:53:41 +0900

    정육면체 두개 합의 최다 확률 값

--- Inflearn_algorithm/section2.py
+++ Inflearn_algorithm/section2.py
@@ -1,1 +1,19 @@
-# 대표값 .
+import sys
+# 두개의 정다면체를 던져 나온 합 중 가장 확률 높은 숫자 출력 (여러개일 경우 오름차순)
+sys.stdin=open("input.txt", "r")
+N, M = map(int, input().split())
+
+answer_list = [0]*(N+M+1)  # 두 수의 합은 2 ~ 두개 정육면체의 최대값 만큼이다
+
+for n in range(1, N+1):
+    for m in range(1, M+1):
+        answer_list[n+m] += 1
+max_answer = -1
+
+for row in answer_list:
+    if row >= max_answer:
+        max_answer = row
+
+for idx, final_row in enumerate(answer_list):
+        if final_row == max_answer:
+             print(idx, end=" ")
