f72b927288e091a727bf117d6506b8f5610297b1
chaehyun <cogus950403@gmail.com>
2024-01-19 00:03:35 +0900

    주어진 시간에 특정 분 더한 시간 구하기기

--- 조건문/time.py
+++ 조건문/time.py
@@ -1,12 +1,21 @@
 # 주어진 시간에서 45분 뺀 시간 구하기
+# 주어진 시간에서 특정시간 구하기
 """
 새로 알게 된 것
 특정 값에 대한 음수값의 나머지는 양수값이 된다.
 """
-h, m = map(int, input())
-new_h = h-(m<45)%24 # 시간은 24시간
-new_m = (m-45)%60 # 분은 60분
+def time_diff():
+    h, m = map(int, input())
+    new_h = h-(m<45)%24 # 시간은 24시간
+    new_m = (m-45)%60 # 분은 60분
 
-print(h-(m<45)%24, (m-45)%60)
+    print(h-(m<45)%24, (m-45)%60)
 
-# 10 10 -> 9 35
+    # 10 10 -> 9 35
+
+def time_add():
+    # h, m = map(int, input())
+    # cook = int(input())
+    h, m = 23, 48
+    cook = 25
+    print((h+((m+cook)//60))%24, (m+cook)%60)
