3f8a6813e27217a5a95208926d5845f50f3231b5
김채현 <cogus950403@gmail.com>
2024-02-27 23:44:14 +0900

    240227 알고리즘 기록 : 3차배열 visit 연습

--- backjoon/14923.py
+++ backjoon/14923.py
@@ -4,49 +4,41 @@ sys.stdin = open("input.txt")
 n, m = map(int, input().split())
 hx, hy = map(int, input().split())
 ex, ey = map(int, input().split())
-
-wall_count = 0
-mirror = []
-for _ in range(n):
-    row = list(map(int, input().split()))
-    wall_count += sum(row)
-    mirror.append(row)
-
+mirror = [list(map(int, input().split())) for _ in range(n)]
+visit = [[[False]*2 for _ in range(m)] for _ in range(n)]
 dx = [-1, 0, 1, 0]
 dy = [0, -1, 0, 1]
 
-def go_mirror(x, y, d, mirror):
-    q = deque()
-    q.append((x, y, d))
-    while q:
-        x, y, d = q.popleft()
-        if x == ex-1 and y == ey-1:
-            return d
-        for i in range(4):
-            xx = x + dx[i]
-            yy = y + dy[i]
-            if 0<= xx < n and 0 <= yy < m and not visit[xx][yy] and mirror[xx][yy] == 0:
-                visit[xx][yy] = True
-                q.append((xx, yy, d+1))
 
-
-short_time = 21740000
-exit_time = []
-for x in range(n):
-    if wall_count == 0:
+q = deque()
+d = 1
+magic = 0
+q.append((hx-1, hy-1, d, magic))
+visit[hx-1][hy-1] = [True, True]
+exit_done = False
+while q:
+    x, y, d, magic = q.popleft()
+    if x == ex-1 and y == ey-1:
+        print(d-1)
+        exit_done = True
+        q.clear()
         break
-    for y in range(m):
-        if mirror[x][y] == 1:
-            wall_count -= 1
-            mirror[x][y] = 0
-            visit = [[False] * m for _ in range(n)]
-            visit[hx-1][hy-1] = True
-            exit_time.append(go_mirror(hx-1, hy-1, 0, mirror))
-            mirror[x][y] = 1
-            if wall_count == 0:
-                break
+    for i in range(4):
+        xx = x + dx[i]
+        yy = y + dy[i]
+        if 0<= xx < n and 0 <= yy < m:
+            # 해당 경우의 수에 대한 방문이력 있음
+            if visit[xx][yy][magic]:
+                continue
+            # 칸이 0 이고 0 방문 안했음
+            if mirror[xx][yy] == 0:
+                visit[xx][yy][magic] = True
+                q.append((xx, yy, d + 1, magic))
+            else:
+                # 마법 안썼다
+                if magic == 0:
+                    visit[xx][yy][1] = True
+                    q.append((xx, yy, d+1, 1))
 
-if len(exit_time) > 0:
-    print(min(exit_time))
-else:
+if not exit_done:
     print(-1)
